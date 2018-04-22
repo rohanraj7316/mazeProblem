81f6c97733a3b390d99a99cc5a486a0c70e4bc8e
Rohan  Raj <rohanraj@Rohans-MacBook-Air.local>
2018-04-23 00:13:55 +0530

    Fix image bug
    images started appering in the maze.

--- src/component/Column.js
+++ src/component/Column.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 
 export default class Column extends Component {
     render () {
-        console.log(this.props);
         const additionalClass = this.props.board[this.props.rowPosition][this.props.colPosition].isHavingFood ? 'cell food' : 'cell';
         return (
-            <div className = 'cell'>
+            <div className = {additionalClass}>
             </div>
         );
     }
